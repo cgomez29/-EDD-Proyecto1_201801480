46cd465b6e6bb290ce28f2c2df977106e6145d72
Cristian Gomez <2912196670108@ingenieria.usac.edu.gt>
2020-08-15 00:44:29 -0600

    Nodo creado

--- Nodo.cpp
+++ Nodo.cpp
@@ -1,1 +1,41 @@
 #include "Nodo.h"
+
+
+Nodo::Nodo()
+{
+    this->next = nullptr;
+    this->previous = nullptr;
+    this->up = nullptr;
+    this->down = nullptr;
+}
+
+void Nodo::setNext(Nodo* next) {
+    this->next = next;
+}
+void Nodo::setUp(Nodo* up) {
+    this->up = up;
+}
+void Nodo::setDown(Nodo* down)
+{
+    this->down = down;
+}
+void Nodo::setPrevious(Nodo* Previous)
+{
+    this->previous = previous;
+}
+Nodo* Nodo::getNext()
+{
+    return this->next;
+}
+Nodo* Nodo::getUp()
+{
+    return this->up;
+}
+Nodo* Nodo::getDown()
+{
+    return this->down;
+}
+Nodo* Nodo::getPrevious()
+{
+    return this->previous;
+}
--- Nodo.h
+++ Nodo.h
@@ -1,5 +1,24 @@
-#pragma once
+#include <iostream>
+
+using namespace std;
+
 class Nodo
 {
-};
+private:
+    Nodo* next;
+    Nodo* previous;
+    Nodo* up;
+    Nodo* down;
 
+public:
+    Nodo();
+    void setNext(Nodo* next);
+    void setPrevious(Nodo* Previous); 
+    void setUp(Nodo* up);
+    void setDown(Nodo* down);
+    Nodo* getNext();
+    Nodo* getUp();
+    Nodo* getDown();
+    Nodo* getPrevious();
+
+};
