76a13bb5bff7d96446795ed4d1ef2ae47f1a0da4
Cristian Gomez <2912196670108@ingenieria.usac.edu.gt>
2020-08-20 22:53:16 -0600

    Modificaciones menores a la clase matrix

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,1 +1,40 @@
 #include "Matrix.h"
+
+Matrix::Matrix()
+{
+	this->horizontal = nullptr;
+	this->vertical = nullptr;
+}
+
+Matrix::~Matrix()
+{
+}
+
+Cabecera* Matrix::crearHorizontal(string x)
+{
+	if (this->horizontal == nullptr)
+	{
+		Cabecera* nueva = new Cabecera(x);
+		this->horizontal = nueva;
+		return nueva;
+	}
+	else
+	{
+	}
+
+}
+Cabecera* Matrix::crearVertical(string y)
+{
+
+}
+
+Cabecera* Matrix::getHorizontal(string x)
+{
+	return this->horizontal;
+
+}
+
+Cabecera* Matrix::getVertical(string y)
+{
+	return this->vertical;
+}
--- Matrix.h
+++ Matrix.h
@@ -1,5 +1,24 @@
 #pragma once
+#include "Cabecera.h"
+#include "Libreria.h"
+
+using namespace std;
+
 class Matrix
 {
+private:
+	Cabecera* horizontal;
+	Cabecera* vertical;
+	Cabecera* crearHorizontal(string x);
+	Cabecera* crearVertical(string y);
+	Cabecera* getHorizontal(string x);
+	Cabecera* getVertical(string y);
+	Nodo* getUltimoV(Cabecera* cabecera, string y);
+	Nodo* getUltimoH(Cabecera* cabecera, string x);
+public:
+	Matrix();
+	~Matrix();
+	void add(Libreria* libreria);
+	void graficar();
 };
 
